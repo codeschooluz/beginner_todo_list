22f6f7a165f2c669ebfbec7ad027cc587ac3a850
ZARIFMAC <uzbzarif@gmail.com>
2022-11-08 17:00:44 +0500

    Get all tasks

--- todoAPI/models.py
+++ todoAPI/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Task(models.Model):
+class TodoItem(models.Model):
     task = models.CharField(max_length=255)
     description = models.TextField()
     status = models.BooleanField(default=False)
--- todoAPI/views.py
+++ todoAPI/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from .models import TodoItem
 
-# Create your views here.
+class TaskView(View):
+    def get(self, request):
+        """
+        Get all tasks
+
+        args:
+            request: HTTP request
+
+        returns:  JSON response      
+        id: int
+        task: string
+        description: string
+        status: boolean
+        created_at: datetime
+        updated_at: datetime
+
+        """
+
+
+    
